a1a50d9726403b0e0b34aa5e917b4ec91e71072b
wangxyPKU <xiaoyuewang@pku.edu.cn>
2018-07-05 09:46:28 +0800

    from servers

--- MultiGPU.cu
+++ MultiGPU.cu
@@ -68,6 +68,8 @@ __global__ void SingleNodeFaiIter(float *fai, float *fai_n, size_t pitch, int H,
 {
 	//unsigned int i = blockDim.y*blockIdx.y + threadIdx.y;
 	//unsigned int j = blockDim.x*blockIdx.x + threadIdx.x;
+	for (int temp = 0; temp < 20; )
+		temp += 1;
 	for (int i = blockDim.y*blockIdx.y + threadIdx.y; i < H; i += blockDim.y*gridDim.y) {
 		float *fai_row_n = (float*)((char*)fai_n + i*pitch);
 		for (int j = blockDim.x*blockIdx.x + threadIdx.x; j < W; j += blockDim.x*gridDim.x) {
@@ -192,7 +194,7 @@ void GpuCalculate(float *fai, int H, int W, int my_rank, int comm_sz)
     }
 
     //Launch the kernel and copy boundary data back. All asynchronously
-    for (n = 0; n < 5000; n++){
+    for (n = 0; n < 10000; n++){
     	for (i = 0; i < GPU_N; i++){
     		//Set device
     		checkCudaErrors(cudaSetDevice(i));
